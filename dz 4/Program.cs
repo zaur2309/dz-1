b6af8d5c8ef293add32c9fedf2e0cd6f163beed2
zaur <zaur_khasanov@bk.ru>
2023-03-14 23:14:43 +0300

    дз1 ко2-му семинару

--- dz 4/Program.cs
+++ dz 4/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write ("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ((a%100)/ 10);
