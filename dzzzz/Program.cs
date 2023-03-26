6f8786cd1e60dc2cfb9095b425f6c90ab9a23b77
zaur <zaur_khasanov@bk.ru>
2023-03-26 17:56:21 +0300

    Дз3 к четвнртому семинару

---
+++ dzzzz/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая задаёт массив из 
+// произвольного кол-ва элементови выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Please, enter the length of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] Array(int size)
+{
+    int [] arr = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Please, enter the number under the index {i}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write("]");
+    System.Console.WriteLine();
+}
+PrintArray(Array(size));
+
+// Все равно не понял как вывести так - [1, 2, 5, 7, 19]
+//Если боьясните в следующем семинаре было бы неплохо)
